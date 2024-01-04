6ed63e5008e270da4dc3ce033dd798f2023012c3
kiran suthar <ksuthar2016@gmail.com>
2024-01-04 15:14:12 +0530

    Setup and Depedencies

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import appLogo from "/ks-blogs.svg";
+
+function App() {
+  return (
+    <main className="bg-red-50">
+      {<img src={appLogo} className="logo" alt="KS Blogs" />}
+    </main>
+  );
+}
+
+export default App;
